e0ea6f4a43bf3c13edc3fd654ea45bb1afa320e5
chappuis.daniel <chappuis.daniel@92aac97c-a6ce-11dd-a772-7fcde58d38e6>
2009-02-02 13:54:37 +0000

    Commit
    git-svn-id: https://reactphysics3d.googlecode.com/svn/trunk@16 92aac97c-a6ce-11dd-a772-7fcde58d38e6

---
+++ sources/demo/Camera.cpp
@@ -0,0 +1,34 @@
+/****************************************************************************
+ * Copyright (C) 2009      Daniel Chappuis                                  *
+ ****************************************************************************
+ * This file is part of ReactPhysics3D.                                     *
+ *                                                                          *
+ * ReactPhysics3D is free software: you can redistribute it and/or modify   *
+ * it under the terms of the GNU General Public License as published by     *
+ * the Free Software Foundation, either version 3 of the License, or        *
+ * (at your option) any later version.                                      *
+ *                                                                          *
+ * ReactPhysics3D is distributed in the hope that it will be useful,        *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the             *
+ * GNU General Public License for more details.                             *
+ *                                                                          *
+ * You should have received a copy of the GNU General Public License        *
+ * along with ReactPhysics3D. If not, see <http://www.gnu.org/licenses/>.   *
+ ***************************************************************************/
+
+// Libraries
+#include "Camera.h"
+
+// Initialization of static variables
+double Camera::speed=0.5;
+
+// Constructor of the camera
+Camera::Camera() {
+
+}
+
+// Destructor of the camera
+Camera::~Camera() {
+
+}
