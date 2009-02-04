5d614981da5c48eec2c9c0c6c9997f9785b6d966
chappuis.daniel <chappuis.daniel@92aac97c-a6ce-11dd-a772-7fcde58d38e6>
2009-02-04 12:10:50 +0000

    git-svn-id: https://reactphysics3d.googlecode.com/svn/trunk@20 92aac97c-a6ce-11dd-a772-7fcde58d38e6

---
+++ sources/reactphysics3d/exceptions.h
@@ -0,0 +1,52 @@
+/****************************************************************************
+ * Copyright (C) 2009      Daniel Chappuis                                  *
+ ****************************************************************************
+ * This file is part of ReactPhysics3D.                                     *
+ *                                                                          *
+ * ReactPhysics3D is free software: you can redistribute it and/or modify   *
+ * it under the terms of the GNU General Public License as published by     *
+ * the Free Software Foundation, either version 3 of the License, or        *
+ * (at your option) any later version.                                      *
+ *                                                                          *
+ * ReactPhysics3D is distributed in the hope that it will be useful,        *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the             *
+ * GNU General Public License for more details.                             *
+ *                                                                          *
+ * You should have received a copy of the GNU General Public License        *
+ * along with ReactPhysics3D. If not, see <http://www.gnu.org/licenses/>.   *
+ ***************************************************************************/
+
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
+
+// Libraries
+#include <stdexcept>
+
+// ReactPhysics3D namespace
+namespace reactphysics3d {
+
+/*  -------------------------------------------------------------------
+    Exception class for the mathematics library
+    -------------------------------------------------------------------
+*/
+
+// Class MathematicsException
+class MathematicsException : public std::runtime_error {
+    public:
+        MathematicsException(const std::string& msg="MathException");           // Constructor
+        virtual ~MathematicsException() throw();                                // Destructor
+        virtual const char* what() const throw();                               // Overriding the base exception method
+};
+
+// Class DivisionByZeroException
+class DivisionByZeroException : public MathematicsException {
+    public:
+        DivisionByZeroException(const std::string& msg="DivisionByZeroException : Division by zero !");     // Constructor
+        virtual ~DivisionByZeroException() throw();                                                         // Destructor
+        virtual const char* what() const throw();                                                           // Overriding the base exception method
+};
+
+} // End of the ReactPhysics3D namespace
+
+#endif
---
+++ sources/reactphysics3d/mathematics.h
@@ -0,0 +1,34 @@
+/****************************************************************************
+ * Copyright (C) 2009      Daniel Chappuis                                  *
+ ****************************************************************************
+ * This file is part of ReactPhysics3D.                                     *
+ *                                                                          *
+ * ReactPhysics3D is free software: you can redistribute it and/or modify   *
+ * it under the terms of the GNU General Public License as published by     *
+ * the Free Software Foundation, either version 3 of the License, or        *
+ * (at your option) any later version.                                      *
+ *                                                                          *
+ * ReactPhysics3D is distributed in the hope that it will be useful,        *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the             *
+ * GNU General Public License for more details.                             *
+ *                                                                          *
+ * You should have received a copy of the GNU General Public License        *
+ * along with ReactPhysics3D. If not, see <http://www.gnu.org/licenses/>.   *
+ ***************************************************************************/
+
+// TODO : Mathematics library : We have to use assert to debug
+// TODO : Mathematics library : Check everywhere that in member methods we use attributes access instead of getter and setter.
+
+#ifndef DCMATHS_H
+#define DCMATHS_H
+
+// Libraries
+#include "Matrix.h"
+#include "Matrix3x3.h"
+#include "Quaternion.h"
+#include "Vector.h"
+#include "Vector3D.h"
+#include "exceptions.h"
+
+#endif
