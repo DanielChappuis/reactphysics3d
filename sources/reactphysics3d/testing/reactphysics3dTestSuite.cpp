cbd19f7e4afd84d8eab91f5a97a647da5f402c93
chappuis.daniel <chappuis.daniel@92aac97c-a6ce-11dd-a772-7fcde58d38e6>
2009-02-04 13:27:46 +0000

    git-svn-id: https://reactphysics3d.googlecode.com/svn/trunk@40 92aac97c-a6ce-11dd-a772-7fcde58d38e6

---
+++ sources/reactphysics3d/testing/reactphysics3dTestSuite.cpp
@@ -0,0 +1,52 @@
+/****************************************************************************
+ * Copyright (C) 2009      Daniel Chappuis                                  *
+ ****************************************************************************
+ * This file is part of ReactPhysics3D.                                     *
+ *                                                                          *
+ * ReactPhysics3D is free software: you can redistribute it and/or modify   *
+ * it under the terms of the GNU General Public License as published by     *
+ * the Free Software Foundation, either version 3 of the License, or        *
+ * (at your option) any later version.                                      *
+ *                                                                          *
+ * ReactPhysics3D is distributed in the hope that it will be useful,        *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the             *
+ *                                                                          *
+ * You should have received a copy of the GNU General Public License        *
+ * along with ReactPhysics3D. If not, see <http://www.gnu.org/licenses/>.   *
+ ***************************************************************************/
+
+// Libraries
+#include <iostream>
+#include "TestSuite/Suite.h"
+#include "mathematics/VectorTest.h"
+#include "mathematics/Vector3DTest.h"
+#include "mathematics/MatrixTest.h"
+#include "mathematics/Matrix3x3Test.h"
+#include "mathematics/QuaternionTest.h"
+
+// Namespaces
+using namespace std;
+using namespace TestSuite;
+
+// Main function
+int main() {
+
+    // ReactPhysics3D TestSuite
+    Suite reactphysics3DTestSuite("ReactPhysics3D TestSuite");
+
+    // Mathematics Tests
+    reactphysics3DTestSuite.addTest(new VectorTest);
+    reactphysics3DTestSuite.addTest(new Vector3DTest);
+    reactphysics3DTestSuite.addTest(new MatrixTest);
+    reactphysics3DTestSuite.addTest(new Matrix3x3Test);
+    reactphysics3DTestSuite.addTest(new QuaternionTest);
+
+    // Run the ReactPhysics3D TestSuite and display the report
+    reactphysics3DTestSuite.run();
+    long nbFailures = reactphysics3DTestSuite.report();
+    reactphysics3DTestSuite.free();
+    return nbFailures;
+    double inPause;
+    cin >> inPause;
+}
