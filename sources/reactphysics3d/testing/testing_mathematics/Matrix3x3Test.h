97756d4ec75cc8675da1d480a8f30198be468943
chappuis.daniel <chappuis.daniel@92aac97c-a6ce-11dd-a772-7fcde58d38e6>
2009-06-05 14:23:25 +0000

    git-svn-id: https://reactphysics3d.googlecode.com/svn/trunk@136 92aac97c-a6ce-11dd-a772-7fcde58d38e6

--- sources/reactphysics3d/testing/testing_mathematics/Matrix3x3Test.h
+++ sources/reactphysics3d/testing/testing_mathematics/Matrix3x3Test.h
@@ -1,20 +1,19 @@
-
 /****************************************************************************
  * Copyright (C) 2009      Daniel Chappuis                                  *
  ****************************************************************************
  * This file is part of ReactPhysics3D.                                     *
  *                                                                          *
  * ReactPhysics3D is free software: you can redistribute it and/or modify   *
- * it under the terms of the GNU General Public License as published by     *
- * the Free Software Foundation, either version 3 of the License, or        *
+ * it under the terms of the GNU Lesser General Public License as published *
+ * by the Free Software Foundation, either version 3 of the License, or     *
  * (at your option) any later version.                                      *
  *                                                                          *
  * ReactPhysics3D is distributed in the hope that it will be useful,        *
  * but WITHOUT ANY WARRANTY; without even the implied warranty of           *
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the             *
- * GNU General Public License for more details.                             *
+ * GNU Lesser General Public License for more details.                      *
  *                                                                          *
- * You should have received a copy of the GNU General Public License        *
+ * You should have received a copy of the GNU Lesser General Public License *
  * along with ReactPhysics3D. If not, see <http://www.gnu.org/licenses/>.   *
  ***************************************************************************/
 
