0cf368e23c8626fe15dc3d74b1a8b8111043c1a0
chappuis.daniel <chappuis.daniel@92aac97c-a6ce-11dd-a772-7fcde58d38e6>
2009-07-21 20:34:52 +0000

    git-svn-id: https://reactphysics3d.googlecode.com/svn/trunk@184 92aac97c-a6ce-11dd-a772-7fcde58d38e6

--- sources/reactphysics3d/reactphysics3d.h
+++ sources/reactphysics3d/reactphysics3d.h
@@ -1,21 +1,21 @@
 /****************************************************************************
- * Copyright (C) 2009      Daniel Chappuis                                  *
- ****************************************************************************
- * This file is part of ReactPhysics3D.                                     *
- *                                                                          *
- * ReactPhysics3D is free software: you can redistribute it and/or modify   *
- * it under the terms of the GNU Lesser General Public License as published *
- * by the Free Software Foundation, either version 3 of the License, or     *
- * (at your option) any later version.                                      *
- *                                                                          *
- * ReactPhysics3D is distributed in the hope that it will be useful,        *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of           *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the             *
- * GNU Lesser General Public License for more details.                      *
- *                                                                          *
- * You should have received a copy of the GNU Lesser General Public License *
- * along with ReactPhysics3D. If not, see <http://www.gnu.org/licenses/>.   *
- ***************************************************************************/
+* Copyright (C) 2009      Daniel Chappuis                                  *
+****************************************************************************
+* This file is part of ReactPhysics3D.                                     *
+*                                                                          *
+* ReactPhysics3D is free software: you can redistribute it and/or modify   *
+* it under the terms of the GNU Lesser General Public License as published *
+* by the Free Software Foundation, either version 3 of the License, or     *
+* (at your option) any later version.                                      *
+*                                                                          *
+* ReactPhysics3D is distributed in the hope that it will be useful,        *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the             *
+* GNU Lesser General Public License for more details.                      *
+*                                                                          *
+* You should have received a copy of the GNU Lesser General Public License *
+* along with ReactPhysics3D. If not, see <http://www.gnu.org/licenses/>.   *
+***************************************************************************/
 
 #ifndef REACTPHYSICS3D_H
 #define REACTPHYSICS3D_H
@@ -23,7 +23,13 @@
 // Libraries
 #include "body/RigidBody.h"
 #include "engine/DynamicWorld.h"
+#include "engine/CollisionWorld.h"
 #include "engine/DynamicEngine.h"
+#include "engine/CollisionEngine.h"
+#include "body/BoundingVolume.h"
+#include "body/OBB.h"
+
+// TODO : We have to include the new files here
 
 // Alias to the ReactPhysics3D namespace
 namespace rp3d = reactphysics3d;
