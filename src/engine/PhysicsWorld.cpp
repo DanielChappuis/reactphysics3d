d762312d689262d458087960804d16be9ea8b63d
chappuis.daniel <chappuis.daniel@92aac97c-a6ce-11dd-a772-7fcde58d38e6>
2011-11-13 17:49:03 +0000

    ReactPhysics3D is now under the ZLib license
    git-svn-id: https://reactphysics3d.googlecode.com/svn/trunk@452 92aac97c-a6ce-11dd-a772-7fcde58d38e6

--- src/engine/PhysicsWorld.cpp
+++ src/engine/PhysicsWorld.cpp
@@ -1,25 +1,26 @@
 /********************************************************************************
 * ReactPhysics3D physics library, http://code.google.com/p/reactphysics3d/      *
-* Copyright (c) 2010 Daniel Chappuis                                            *
+* Copyright (c) 2010-2012 Daniel Chappuis                                       *
 *********************************************************************************
 *                                                                               *
-* Permission is hereby granted, free of charge, to any person obtaining a copy  *
-* of this software and associated documentation files (the "Software"), to deal *
-* in the Software without restriction, including without limitation the rights  *
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell     *
-* copies of the Software, and to permit persons to whom the Software is         *
-* furnished to do so, subject to the following conditions:                      *
+* This software is provided 'as-is', without any express or implied warranty.   *
+* In no event will the authors be held liable for any damages arising from the  *
+* use of this software.                                                         *
 *                                                                               *
-* The above copyright notice and this permission notice shall be included in    *
-* all copies or substantial portions of the Software.                           *
+* Permission is granted to anyone to use this software for any purpose,         *
+* including commercial applications, and to alter it and redistribute it        *
+* freely, subject to the following restrictions:                                *
 *                                                                               *
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR    *
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,      *
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE   *
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER        *
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, *
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN     *
-* THE SOFTWARE.                                                                 *
+* 1. The origin of this software must not be misrepresented; you must not claim *
+*    that you wrote the original software. If you use this software in a        *
+*    product, an acknowledgment in the product documentation would be           *
+*    appreciated but is not required.                                           *
+*                                                                               *
+* 2. Altered source versions must be plainly marked as such, and must not be    *
+*    misrepresented as being the original software.                             *
+*                                                                               *
+* 3. This notice may not be removed or altered from any source distribution.    *
+*                                                                               *
 ********************************************************************************/
 
 // Libraries
@@ -33,7 +34,7 @@ using namespace std;
 
 // Constructor
 PhysicsWorld::PhysicsWorld(const Vector3& gravity)
-             : gravity(gravity), isGravityOn(true), currentBodyID(0) {
+             : gravity(gravity), isGravityOn(true), currentBodyID(0), memoryPoolRigidBodies(NB_MAX_BODIES) {
 
 }
 
@@ -44,10 +45,9 @@ PhysicsWorld::~PhysicsWorld() {
 
 // Create a rigid body into the physics world
 RigidBody* PhysicsWorld::createRigidBody(const Transform& transform, double mass, const Matrix3x3& inertiaTensorLocal, Shape* shape) {
-    // TODO : Use Memory Pool memory allocation for the rigid body instead
     
     // Create the rigid body
-    RigidBody* rigidBody = new RigidBody(transform, mass, inertiaTensorLocal, shape, currentBodyID);
+    RigidBody* rigidBody = new (memoryPoolRigidBodies.allocateObject()) RigidBody(transform, mass, inertiaTensorLocal, shape, currentBodyID);
     
     currentBodyID++;
     
@@ -59,7 +59,12 @@ RigidBody* PhysicsWorld::createRigidBody(const Transform& transform, double mass
 // Destroy a rigid body
 void PhysicsWorld::destroyRigidBody(RigidBody* rigidBody) {
     removeRigidBody(rigidBody);
-    delete rigidBody;
+	
+	// Call the constructor of the rigid body
+	rigidBody->RigidBody::~RigidBody();
+	
+	// Free the object from the memory pool
+	memoryPoolRigidBodies.freeObject(rigidBody);
 }  
 
 // Remove all collision contacts constraints
