2fb5bb47214735996b547826a1a00a17788d33c2
James Gangur <james.gangur@gmail.com>
2015-06-21 23:23:40 +0930

    Rearranged the entire source tree. There is now a seperate include directory, containing a reactphysics3d subdirectory, where the headers live. This greatly eases intergration of the library into other projects, as well as prevent naming confilcts. All reactphysics3d source and headers have been modified to use the new paths. The testbed application has not been changed, as this will not break existing client code. Fixed a couple of comments, formatting issues, etc that I happened to come across.

---
+++ include/reactphysics3d/body/CollisionBody.h
@@ -0,0 +1,303 @@
+/********************************************************************************
+* ReactPhysics3D physics library, http://www.reactphysics3d.com                 *
+* Copyright (c) 2010-2015 Daniel Chappuis                                       *
+*********************************************************************************
+*                                                                               *
+* This software is provided 'as-is', without any express or implied warranty.   *
+* In no event will the authors be held liable for any damages arising from the  *
+* use of this software.                                                         *
+*                                                                               *
+* Permission is granted to anyone to use this software for any purpose,         *
+* including commercial applications, and to alter it and redistribute it        *
+* freely, subject to the following restrictions:                                *
+*                                                                               *
+* 1. The origin of this software must not be misrepresented; you must not claim *
+*    that you wrote the original software. If you use this software in a        *
+*    product, an acknowledgment in the product documentation would be           *
+*    appreciated but is not required.                                           *
+*                                                                               *
+* 2. Altered source versions must be plainly marked as such, and must not be    *
+*    misrepresented as being the original software.                             *
+*                                                                               *
+* 3. This notice may not be removed or altered from any source distribution.    *
+*                                                                               *
+********************************************************************************/
+
+#ifndef REACTPHYSICS3D_COLLISION_BODY_H
+#define REACTPHYSICS3D_COLLISION_BODY_H
+
+// Libraries
+#include <cassert>
+#include <stdexcept>
+#include "reactphysics3d/configuration.h"
+#include "reactphysics3d/body/Body.h"
+#include "reactphysics3d/collision/shapes/AABB.h"
+#include "reactphysics3d/collision/shapes/CollisionShape.h"
+#include "reactphysics3d/collision/RaycastInfo.h"
+#include "reactphysics3d/mathematics/Transform.h"
+#include "reactphysics3d/memory/MemoryAllocator.h"
+
+/// ReactPhysics3D Namespace
+namespace reactphysics3d {
+
+// Class declarations
+struct ContactManifoldListElement;
+class ProxyShape;
+class CollisionWorld;
+
+/// Enumeration for the type of a body
+/// STATIC : A static body has infinite mass, zero velocity but the position can be
+///          changed manually. A static body does not collide with other static or kinematic bodies.
+/// KINEMATIC : A kinematic body has infinite mass, the velocity can be changed manually and its
+///             position is computed by the physics engine. A kinematic body does not collide with
+///             other static or kinematic bodies.
+/// DYNAMIC : A dynamic body has non-zero mass, non-zero velocity determined by forces and its
+///           position is determined by the physics engine. A dynamic body can collide with other
+///           dynamic, static or kinematic bodies.
+enum BodyType {STATIC, KINEMATIC, DYNAMIC};
+
+// Class CollisionBody
+/**
+ * This class represents a body that is able to collide with others
+ * bodies. This class inherits from the Body class.
+ */
+class CollisionBody : public Body {
+
+    protected :
+
+        // -------------------- Attributes -------------------- //
+
+        /// Type of body (static, kinematic or dynamic)
+        BodyType mType;
+
+        /// Position and orientation of the body
+        Transform mTransform;
+
+        /// First element of the linked list of proxy collision shapes of this body
+        ProxyShape* mProxyCollisionShapes;
+
+        /// Number of collision shapes
+        uint mNbCollisionShapes;
+
+        /// First element of the linked list of contact manifolds involving this body
+        ContactManifoldListElement* mContactManifoldsList;
+
+        /// Reference to the world the body belongs to
+        CollisionWorld& mWorld;
+
+        // -------------------- Methods -------------------- //
+
+        /// Private copy-constructor
+        CollisionBody(const CollisionBody& body);
+
+        /// Private assignment operator
+        CollisionBody& operator=(const CollisionBody& body);
+
+        /// Reset the contact manifold lists
+        void resetContactManifoldsList();
+
+        /// Remove all the collision shapes
+        void removeAllCollisionShapes();
+
+        /// Update the broad-phase state for this body (because it has moved for instance)
+        virtual void updateBroadPhaseState() const;
+
+        /// Ask the broad-phase to test again the collision shapes of the body for collision
+        /// (as if the body has moved).
+        void askForBroadPhaseCollisionCheck() const;
+
+        /// Reset the mIsAlreadyInIsland variable of the body and contact manifolds
+        int resetIsAlreadyInIslandAndCountManifolds();
+
+    public :
+
+        // -------------------- Methods -------------------- //
+
+        /// Constructor
+        CollisionBody(const Transform& transform, CollisionWorld& world, bodyindex id);
+
+        /// Destructor
+        virtual ~CollisionBody();
+
+        /// Return the type of the body
+        BodyType getType() const;
+
+        /// Set the type of the body
+        void setType(BodyType type);
+
+        /// Set whether or not the body is active
+        virtual void setIsActive(bool isActive);
+
+        /// Return the current position and orientation
+        const Transform& getTransform() const;
+
+        /// Set the current position and orientation
+        void setTransform(const Transform& transform);
+
+        /// Add a collision shape to the body.
+        virtual ProxyShape* addCollisionShape(const CollisionShape& collisionShape,
+                                      const Transform& transform);
+
+        /// Remove a collision shape from the body
+        virtual void removeCollisionShape(const ProxyShape* proxyShape);
+
+        /// Return the first element of the linked list of contact manifolds involving this body
+        const ContactManifoldListElement* getContactManifoldsList() const;
+
+        /// Return true if a point is inside the collision body
+        bool testPointInside(const Vector3& worldPoint) const;
+
+        /// Raycast method with feedback information
+        bool raycast(const Ray& ray, RaycastInfo& raycastInfo);
+
+        /// Compute and return the AABB of the body by merging all proxy shapes AABBs
+        AABB getAABB() const;
+
+        /// Return the linked list of proxy shapes of that body
+        ProxyShape* getProxyShapesList();
+
+        /// Return the linked list of proxy shapes of that body
+        const ProxyShape* getProxyShapesList() const;
+
+        /// Return the world-space coordinates of a point given the local-space coordinates of the body
+        Vector3 getWorldPoint(const Vector3& localPoint) const;
+
+        /// Return the world-space vector of a vector given in local-space coordinates of the body
+        Vector3 getWorldVector(const Vector3& localVector) const;
+
+        /// Return the body local-space coordinates of a point given in the world-space coordinates
+        Vector3 getLocalPoint(const Vector3& worldPoint) const;
+
+        /// Return the body local-space coordinates of a vector given in the world-space coordinates
+        Vector3 getLocalVector(const Vector3& worldVector) const;
+
+        // -------------------- Friendship -------------------- //
+
+        friend class CollisionWorld;
+        friend class DynamicsWorld;
+        friend class CollisionDetection;
+        friend class BroadPhaseAlgorithm;
+        friend class ConvexMeshShape;
+        friend class ProxyShape;
+};
+
+// Return the type of the body
+/**
+ * @return the type of the body (STATIC, KINEMATIC, DYNAMIC)
+ */
+inline BodyType CollisionBody::getType() const {
+    return mType;
+}
+
+// Set the type of the body
+/// The type of the body can either STATIC, KINEMATIC or DYNAMIC as described bellow:
+/// STATIC : A static body has infinite mass, zero velocity but the position can be
+///          changed manually. A static body does not collide with other static or kinematic bodies.
+/// KINEMATIC : A kinematic body has infinite mass, the velocity can be changed manually and its
+///             position is computed by the physics engine. A kinematic body does not collide with
+///             other static or kinematic bodies.
+/// DYNAMIC : A dynamic body has non-zero mass, non-zero velocity determined by forces and its
+///           position is determined by the physics engine. A dynamic body can collide with other
+///           dynamic, static or kinematic bodies.
+/**
+ * @param type The type of the body (STATIC, KINEMATIC, DYNAMIC)
+ */
+inline void CollisionBody::setType(BodyType type) {
+    mType = type;
+
+    if (mType == STATIC) {
+
+        // Update the broad-phase state of the body
+        updateBroadPhaseState();
+    }
+}
+
+// Return the current position and orientation
+/**
+ * @return The current transformation of the body that transforms the local-space
+ *         of the body into world-space
+ */
+inline const Transform& CollisionBody::getTransform() const {
+    return mTransform;
+}
+
+// Set the current position and orientation
+/**
+ * @param transform The transformation of the body that transforms the local-space
+ *                  of the body into world-space
+ */
+inline void CollisionBody::setTransform(const Transform& transform) {
+
+    // Update the transform of the body
+    mTransform = transform;
+
+    // Update the broad-phase state of the body
+    updateBroadPhaseState();
+}
+
+// Return the first element of the linked list of contact manifolds involving this body
+/**
+ * @return A pointer to the first element of the linked-list with the contact
+ *         manifolds of this body
+ */
+inline const ContactManifoldListElement* CollisionBody::getContactManifoldsList() const {
+    return mContactManifoldsList;
+}
+
+// Return the linked list of proxy shapes of that body
+/**
+* @return The pointer of the first proxy shape of the linked-list of all the
+*         proxy shapes of the body
+*/
+inline ProxyShape* CollisionBody::getProxyShapesList() {
+    return mProxyCollisionShapes;
+}
+
+// Return the linked list of proxy shapes of that body
+/**
+* @return The pointer of the first proxy shape of the linked-list of all the
+*         proxy shapes of the body
+*/
+inline const ProxyShape* CollisionBody::getProxyShapesList() const {
+    return mProxyCollisionShapes;
+}
+
+// Return the world-space coordinates of a point given the local-space coordinates of the body
+/**
+* @param localPoint A point in the local-space coordinates of the body
+* @return The point in world-space coordinates
+*/
+inline Vector3 CollisionBody::getWorldPoint(const Vector3& localPoint) const {
+    return mTransform * localPoint;
+}
+
+// Return the world-space vector of a vector given in local-space coordinates of the body
+/**
+* @param localVector A vector in the local-space coordinates of the body
+* @return The vector in world-space coordinates
+*/
+inline Vector3 CollisionBody::getWorldVector(const Vector3& localVector) const {
+    return mTransform.getOrientation() * localVector;
+}
+
+// Return the body local-space coordinates of a point given in the world-space coordinates
+/**
+* @param worldPoint A point in world-space coordinates
+* @return The point in the local-space coordinates of the body
+*/
+inline Vector3 CollisionBody::getLocalPoint(const Vector3& worldPoint) const {
+    return mTransform.getInverse() * worldPoint;
+}
+
+// Return the body local-space coordinates of a vector given in the world-space coordinates
+/**
+* @param worldVector A vector in world-space coordinates
+* @return The vector in the local-space coordinates of the body
+*/
+inline Vector3 CollisionBody::getLocalVector(const Vector3& worldVector) const {
+    return mTransform.getOrientation().getInverse() * worldVector;
+}
+
+}
+
+ #endif
---
+++ include/reactphysics3d/configuration.h
@@ -0,0 +1,133 @@
+/********************************************************************************
+* ReactPhysics3D physics library, http://www.reactphysics3d.com                 *
+* Copyright (c) 2010-2015 Daniel Chappuis                                       *
+*********************************************************************************
+*                                                                               *
+* This software is provided 'as-is', without any express or implied warranty.   *
+* In no event will the authors be held liable for any damages arising from the  *
+* use of this software.                                                         *
+*                                                                               *
+* Permission is granted to anyone to use this software for any purpose,         *
+* including commercial applications, and to alter it and redistribute it        *
+* freely, subject to the following restrictions:                                *
+*                                                                               *
+* 1. The origin of this software must not be misrepresented; you must not claim *
+*    that you wrote the original software. If you use this software in a        *
+*    product, an acknowledgment in the product documentation would be           *
+*    appreciated but is not required.                                           *
+*                                                                               *
+* 2. Altered source versions must be plainly marked as such, and must not be    *
+*    misrepresented as being the original software.                             *
+*                                                                               *
+* 3. This notice may not be removed or altered from any source distribution.    *
+*                                                                               *
+********************************************************************************/
+
+#ifndef REACTPHYSICS3D_CONFIGURATION_H
+#define	REACTPHYSICS3D_CONFIGURATION_H
+
+// Libraries
+#include <cfloat>
+#include <limits>
+#include <utility>
+#include "reactphysics3d/decimal.h"
+
+// Windows platform
+#if defined(WIN32) ||defined(_WIN32) || defined(_WIN64) ||defined(__WIN32__) || defined(__WINDOWS__)
+    #define WINDOWS_OS
+#elif defined(__APPLE__)     // Apple platform
+    #define APPLE_OS
+#elif defined(__linux__) || defined(linux) || defined(__linux)    // Linux platform
+    #define LINUX_OS
+#endif
+
+/// ReactPhysics3D Namespace
+namespace reactphysics3d {
+
+// ------------------- Type definitions ------------------- //
+
+typedef unsigned int uint;
+typedef long unsigned int luint;
+typedef luint bodyindex;
+typedef std::pair<bodyindex, bodyindex> bodyindexpair;
+
+// ------------------- Enumerations ------------------- //
+
+/// Position correction technique used in the constraint solver (for joints).
+/// BAUMGARTE_JOINTS : Faster but can be innacurate in some situations.
+/// NON_LINEAR_GAUSS_SEIDEL : Slower but more precise. This is the option used by default.
+enum JointsPositionCorrectionTechnique {BAUMGARTE_JOINTS, NON_LINEAR_GAUSS_SEIDEL};
+
+/// Position correction technique used in the contact solver (for contacts)
+/// BAUMGARTE_CONTACTS : Faster but can be innacurate and can lead to unexpected bounciness
+///                      in some situations (due to error correction factor being added to
+///                      the bodies momentum).
+/// SPLIT_IMPULSES : A bit slower but the error correction factor is not added to the
+///                 bodies momentum. This is the option used by default.
+enum ContactsPositionCorrectionTechnique {BAUMGARTE_CONTACTS, SPLIT_IMPULSES};
+
+// ------------------- Constants ------------------- //
+
+/// Smallest decimal value (negative)
+const decimal DECIMAL_SMALLEST = - std::numeric_limits<decimal>::max();
+
+/// Maximum decimal value
+const decimal DECIMAL_LARGEST = std::numeric_limits<decimal>::max();
+
+/// Machine epsilon
+const decimal MACHINE_EPSILON = std::numeric_limits<decimal>::epsilon();
+
+/// Pi constant
+const decimal PI = decimal(3.14159265);
+
+/// 2*Pi constant
+const decimal PI_TIMES_2 = decimal(6.28318530);
+
+/// Default friction coefficient for a rigid body
+const decimal DEFAULT_FRICTION_COEFFICIENT = decimal(0.3);
+
+/// Default bounciness factor for a rigid body
+const decimal DEFAULT_BOUNCINESS = decimal(0.5);
+
+/// True if the spleeping technique is enabled
+const bool SPLEEPING_ENABLED = true;
+
+/// Object margin for collision detection in meters (for the GJK-EPA Algorithm)
+const decimal OBJECT_MARGIN = decimal(0.04);
+
+/// Distance threshold for two contact points for a valid persistent contact (in meters)
+const decimal PERSISTENT_CONTACT_DIST_THRESHOLD = decimal(0.03);
+
+/// Velocity threshold for contact velocity restitution
+const decimal RESTITUTION_VELOCITY_THRESHOLD = decimal(1.0);
+
+/// Number of iterations when solving the velocity constraints of the Sequential Impulse technique
+const uint DEFAULT_VELOCITY_SOLVER_NB_ITERATIONS = 10;
+
+/// Number of iterations when solving the position constraints of the Sequential Impulse technique
+const uint DEFAULT_POSITION_SOLVER_NB_ITERATIONS = 5;
+
+/// Time (in seconds) that a body must stay still to be considered sleeping
+const float DEFAULT_TIME_BEFORE_SLEEP = 1.0f;
+
+/// A body with a linear velocity smaller than the sleep linear velocity (in m/s)
+/// might enter sleeping mode.
+const decimal DEFAULT_SLEEP_LINEAR_VELOCITY = decimal(0.02);
+
+/// A body with angular velocity smaller than the sleep angular velocity (in rad/s)
+/// might enter sleeping mode
+const decimal DEFAULT_SLEEP_ANGULAR_VELOCITY = decimal(3.0 * (PI / 180.0));
+
+/// In the broad-phase collision detection (dynamic AABB tree), the AABBs are
+/// inflated with a constant gap to allow the collision shape to move a little bit
+/// without triggering a large modification of the tree which can be costly
+const decimal DYNAMIC_TREE_AABB_GAP = decimal(0.1);
+
+/// In the broad-phase collision detection (dynamic AABB tree), the AABBs are
+/// also inflated in direction of the linear motion of the body by mutliplying the
+/// followin constant with the linear velocity and the elapsed time between two frames.
+const decimal DYNAMIC_TREE_AABB_LIN_GAP_MULTIPLIER = decimal(1.7);
+
+}
+
+#endif
