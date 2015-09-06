a928c73827ef1b5c08d69d35c4bdb33d3d35d9a3
Daniel Chappuis <chappuis.daniel@gmail.com>
2015-09-06 12:38:10 +0200

    Fix compilation errors on Visual Studio

--- testbed/common/Capsule.cpp
+++ testbed/common/Capsule.cpp
@@ -238,7 +238,7 @@ void Capsule::createVBOAndVAO() {
     // Create th VBO for the indices data
     mVBOIndices.create();
     mVBOIndices.bind();
-    size_t sizeIndices = mIndices[0].size() * sizeof(uint);
+    size_t sizeIndices = mIndices[0].size() * sizeof(unsigned int);
     mVBOIndices.copyDataIntoVBO(sizeIndices, getIndicesPointer(), GL_STATIC_DRAW);
     mVBOIndices.unbind();
 
