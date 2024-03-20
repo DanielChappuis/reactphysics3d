4c0ffb6418b88db4c8219e4d3c37b6f6cace5307
Daniel Chappuis <chappuis.daniel@gmail.com>
2024-03-20 23:50:26 +0100

    Working on documentation

---
+++ documentation/README.md
@@ -0,0 +1,15 @@
+
+ This file describes how the documentation of the project is generated.
+
+ Reference: https://devblogs.microsoft.com/cppblog/clear-functional-c-documentation-with-sphinx-breathe-doxygen-cmake
+
+ # Dependencies
+
+  - Doxygen
+  - Sphinx
+  - Breathe (bridge between Doxygen and Sphinx)
+  - CMake
+
+ # How to generate the documentation
+
+
---
+++ documentation/sphinx/source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'ReactPhysics3D'
+copyright = '2024, Daniel Chappuis'
+author = 'Daniel Chappuis'
+release = 'v0.10.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["breathe"]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# Breathe Configuration
+breathe_default_project = "ReactPhysics3D"
+
+highlight_language = 'c++'
